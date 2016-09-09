e55958ce790c03394da9fbbbdb4eb737088ce09e
sanjayradadiya <sanjay.radadiya91@gmail.com>
2016-09-09 11:00:47 +0530

    config variable added

--- index.php
+++ index.php
@@ -4,14 +4,15 @@
 	<title>my first heroku test</title>
 </head>
 <body>
-<table>
+<table border="1">
 <tr>
 	<td>Elements</td>
 </tr>
 <?php
+   $my_env_var = getenv('SANJAY');
    for($i=0;$i<=10;$i++)
    {
-      echo '<tr><td>'. $i. ' sanjay'.'</td></tr>';
+      echo '<tr><td>'. $i. $my_env_var .'</td></tr>';
    }
 ?>
 </table>
